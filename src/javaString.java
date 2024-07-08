5a629d52ac4d5989646794ad79ec1263c1d8246b
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-07-08 22:49:26 +0600

    java string

---
+++ src/javaString.java
@@ -0,0 +1,7 @@
+public class javaString {
+    public static void main(String[] args) {
+        String str="hello world";
+        System.out.println(str.length());//string length
+        System.out.println(str.toUpperCase());//convert all letter to uppercase
+    }
+}
