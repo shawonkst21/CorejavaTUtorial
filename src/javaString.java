5a09083ccb7ecdfba3b71796d6c5938f8e9a836c
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-07-08 22:53:23 +0600

    learn once more string function

--- src/javaString.java
+++ src/javaString.java
@@ -1,7 +1,9 @@
 public class javaString {
     public static void main(String[] args) {
         String str="hello world";
+        String str2="HELLO WORLD";
         System.out.println(str.length());//string length
         System.out.println(str.toUpperCase());//convert all letter to uppercase
+        System.out.println(str.toLowerCase());
     }
 }
