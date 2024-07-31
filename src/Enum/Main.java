d8d1200871aa462d1843b614081c72adf92b2575
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-07-31 12:22:11 +0600

    enum class

--- src/Enum/Main.java
+++ src/Enum/Main.java
@@ -1,16 +1,28 @@
 package Enum;
-enum level{
-    running,pending,success
+enum laptop{
+
+    macbook(22000),hp(1122),dell(3444),thikpadd(300),sufrface(8999);
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    private laptop(int price)
+    {
+        this.price=price;
+    }
 }
 public class Main {
     public static void main(String[] args) {
-        //System.out.println(level.pending);
-        for(level i:level.values())
+        for(laptop lap:laptop.values())
         {
-            if(i==level.success)
-            {
-                System.out.println("yea hu you are success");
-            }
+            System.out.println(lap+":"+lap.getPrice());
         }
+
     }
 }
