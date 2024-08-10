de9f702f3abd5d6690284f9ccf55fdacec7f2da6
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-08-10 07:39:46 +0600

    return value lamda expression

--- src/lamdaEx/Main.java
+++ src/lamdaEx/Main.java
@@ -2,14 +2,18 @@ package lamdaEx;
 @FunctionalInterface
 interface A
 {
-    void show(int x);
+    int show(int x,int y);
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        A obj=(int x)-> System.out.println("in show mwthod "+x);
-        obj.show(5);
+        A obj=(int x,int y)-> {
+            return x + y;
+        };
+       int res= obj.show(5,8);
+        System.out.println(res);
+
 
     }
 }
