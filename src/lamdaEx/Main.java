d6bf9e01f553ff3655fa152b32afc3e440f5f3fa
Sadman-Shawon <156732410+shawonkst21@users.noreply.github.com>
2024-08-10 07:32:52 +0600

    shoter this code using lamda

--- src/lamdaEx/Main.java
+++ src/lamdaEx/Main.java
@@ -2,18 +2,14 @@ package lamdaEx;
 @FunctionalInterface
 interface A
 {
-    void show();
+    void show(int x);
 }
 
 
 public class Main {
     public static void main(String[] args) {
-        A obj=()->
-        {
-                System.out.println("in show mwthod");
-        };
-
-        obj.show();
+        A obj=(int x)-> System.out.println("in show mwthod "+x);
+        obj.show(5);
 
     }
 }
